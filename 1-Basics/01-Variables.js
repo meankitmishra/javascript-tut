e7ebfa8393a7104e16d376b7328ba7e6246d1059
meankitmishra <119694523+meankitmishra@users.noreply.github.com>
2023-12-22 18:10:29 +0000

    1st day

---
+++ 1-Basics/01-Variables.js
@@ -0,0 +1,13 @@
+const Id = 14545
+let accountName = "Ankit"
+var accountEmail = "ankitmishra@xyz.com"
+accountLocation = "Delhi"
+
+// Id = 26262 // cant reassign the value of a const 
+
+console.log(Id);
+
+/*
+    we dont prefer to use var as it causes problems in scope programming and function scope
+*/
+console.table([Id , accountName , accountEmail, accountLocation])
---
+++ 1-Basics/02-DataTypes.js
@@ -0,0 +1,15 @@
+// Primitive datatypes
+
+//number
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol
+
+//array
+//object
+
+// null is of object type
+// undefiend is undefined only
