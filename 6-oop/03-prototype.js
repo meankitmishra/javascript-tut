70d4d3a9de55a3955a0bd47fbef7bf6e8d56aa80
meankitmishra <119694523+meankitmishra@users.noreply.github.com>
2024-01-06 17:28:29 +0000

    completed for time being ig

---
+++ 6-oop/03-prototype.js
@@ -0,0 +1,47 @@
+let myName = "Ankit       "
+
+console.log(myName.length);
+
+let myHeros = ['thor', 'spiderman']
+
+let heroPowers = {
+    thor:'hammer',
+    spiderman:'slinky',
+
+    getSpiderPowers: function(){
+        console.log(`spider power is ${this.spiderman}`);
+    }
+}
+
+Object.defineProperty(String.prototype,'trueLength',{
+    get: function(){
+        return this.trim().length
+    }
+})
+
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+console.log(myName.trueLength); 
